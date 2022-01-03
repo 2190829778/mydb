6e9958f7e866f8edfbe5bd018730b13d8498ef52
lepingyang <lepingyang@trip.com>
2022-01-03 16:20:18 +0800

    初始化maven项目

---
+++ src/main/java/com/yangleping/mydb/App.java
@@ -0,0 +1,12 @@
+package com.yangleping.mydb;
+
+/**
+ * @package:com.yangleping.mydb
+ * @user LePingYang
+ * @date 2022/1/3
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
